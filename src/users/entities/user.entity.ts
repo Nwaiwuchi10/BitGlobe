5fb299feee029aacd788d9c2de6387a4a77290ff
Nwaiwuchi10 <djnchrys@gmail.com>
2025-09-02 13:08:18 -0700

    main

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -35,6 +35,9 @@ export class User extends Document {
 
   @Prop({ default: false })
   isBlocked: boolean; // block login
+
+  @Prop({ default: false })
+  isAdmin: boolean;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
